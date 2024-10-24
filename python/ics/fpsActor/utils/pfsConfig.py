416d705b5985709e5cf095bfd2eb2b8586dbfa4e
Arnaud Le Fur <afur@astro.princeton.edu>
2024-10-23 23:39:05 -1000

    fix indentation error

--- python/ics/fpsActor/utils/pfsConfig.py
+++ python/ics/fpsActor/utils/pfsConfig.py
@@ -26,7 +26,7 @@ def pfsConfigFromDesign(pfsDesign, visit0, header=None, maskFile=None):
         noTargetMask = np.logical_and(noEng, np.isin(pfsDesign.fiberId, noTarget.fiberId))
         pfsDesign.targetType[noTargetMask] = TargetType.UNASSIGNED
 
-        return PfsConfig.fromPfsDesign(pfsDesign, visit0, pfsDesign.pfiNominal, header=header)
+    return PfsConfig.fromPfsDesign(pfsDesign, visit0, pfsDesign.pfiNominal, header=header)
 
 
 def makeTargetsArray(pfsConfig):
