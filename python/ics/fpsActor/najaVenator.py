94690c9dd78987110808c3d7ca0e69a816dbce90
Craig Loomis <cloomis@astro.princeton.edu>
2025-12-18 17:44:02 -1000

    Convert opdb access to pfs.utils.database

--- python/ics/fpsActor/najaVenator.py
+++ python/ics/fpsActor/najaVenator.py
@@ -4,10 +4,7 @@ from importlib import reload
 import numpy as np
 import pandas as pd
 import psycopg2
-from opdb import opdb
-
-reload(opdb)
-
+from pfs.utils.database import opdb
 
 class NajaVenator(object):
     """ 
@@ -17,73 +14,37 @@ class NajaVenator(object):
     """
 
     def __init__(self):
-        self._dbConn = opdb.OpDB(hostname='db-ics', dbname='opdb', username='pfs')
+        self._dbConn = opdb.OpDB()
 
     @staticmethod
     def connect():
         """Return connection object, password needs to be defined in /home/user/.pgpass."""
         return psycopg2.connect(host='db-ics', dbname='opdb', user='pfs')
 
+    def reform(self, df, nameMap=None, typeMap=None):
+        if nameMap is not None:
+            colNames = nameMap.keys()
+            df = df[colNames].rename(columns=nameMap, inplace=True)
+
+        if typeMap is not None:
+            df = df.astype(typeMap)
+
+        return df
+
     def readFFConfig(self):
         """Read positions of all fidicial fibers."""
-        buf = io.StringIO()
-        cmd = f"""COPY (SELECT * from fiducial_fiber_geometry WHERE fiducial_fiber_calib_id = 1) to stdout delimiter ','"""
 
-        with self.connect() as conn:
-            with conn.cursor() as curs:
-                curs.copy_expert(cmd, buf)
-        buf.seek(0, 0)
-
-        # Skip the frameId, etc. columns.
-        arr = np.genfromtxt(buf, dtype='f4',
-                            delimiter=',', usecols=(0, 1, 2))
-
-        d = {'fiberID': arr[:, 0].astype('int32'), 'x': arr[:, 1].astype('float'), 'y': arr[:, 2].astype('float')}
-
-        return pd.DataFrame(d)
-
-    def readCobraConfig(self):
-        """Read positions of all science fibers."""
-        buf = io.StringIO()
-        cmd = f"""COPY (SELECT * from "FiberPosition" WHERE "ftype" = 'cobra') to stdout delimiter ','"""
-
-        with self.connect() as conn:
-            with conn.cursor() as curs:
-                curs.copy_expert(cmd, buf)
-        buf.seek(0, 0)
-
-        # Skip the frameId, etc. columns.
-        arr = np.genfromtxt(buf, dtype='f4',
-                            delimiter=',', usecols=(0, 1, 2, 3))
-
-        d = {'fiberID': arr[:, 0].astype('int32'), 'x': arr[:, 2].astype('float'), 'y': arr[:, 3].astype('int32')}
-
-        return pd.DataFrame(d)
-
-    def readCentroidOld(self, frameId):
-        """ Read centroid information from databse"""
-
-        buf = io.StringIO()
-
-        cmd = f"""COPY (SELECT mcs_frame_id, spot_id, mcs_center_x_pix, mcs_center_y_pix from mcs_data WHERE mcs_frame_id={frameId}) to stdout delimiter ','"""
-
-        with self.connect() as conn:
-            with conn.cursor() as curs:
-                curs.copy_expert(cmd, buf)
-        buf.seek(0, 0)
-
-        # Skip the frameId, etc. columns.
-        arr = np.genfromtxt(buf, dtype=[("mcsId", 'i4'), ("fiberID", 'i4'), ("centroidx", 'f4'), ("centroidy", 'f4')],
-                            delimiter=',', usecols=(0, 1, 2, 3))
-
-        return pd.DataFrame(arr)
+        df = self._dbConn.query_dataframe("SELECT fiducial_fiber_id as id, ff_center_on_pfi_x_mm as x, ff_center_on_pfi_y_mm as y "
+                                          "FROM fiducial_fiber_geometry WHERE fiducial_fiber_calib_id = 1")
+        df = self.reform(typeMap=dict(id='i4',
+                                       x='f4',
+                                       y='f4'))
+        return df
 
     def readCentroid(self, frameId):
         """ Read centroid information from database. This requires INSTRM-1110."""
-        conn = self._dbConn
 
-        sql = f"""SELECT * from mcs_data WHERE mcs_frame_id={frameId}"""
-        df = conn.bulkSelect('mcs_data', sql)
+        df = self._dbConn.query_dataframe(f"""SELECT * from mcs_data WHERE mcs_frame_id={frameId}""")
 
         # We got a full table, with original names. Trim and rename to
         # what is expected here.
@@ -92,11 +53,14 @@ class NajaVenator(object):
                        mcs_center_x_pix='centroidx',
                        mcs_center_y_pix='centroidy')
 
-        df = df[renames.keys()]
-        df.rename(columns=renames, inplace=True)
+        df = self.reform(nameMap=renames,
+                         typeMap=dict(centroidx='f4',
+                                      centroidy='f4'))
         return df
 
     def readTelescopeInform(self, frameId):
+        raise NotImplementedError()
+
         buf = io.StringIO()
 
         cmd = f"""COPY (SELECT * from mcs_exposure WHERE mcs_frame_id={frameId}) to stdout delimiter ','"""
@@ -120,6 +84,8 @@ class NajaVenator(object):
                 curs.execute(cmd)
 
     def writeCobraConfig(self, matchCatalog, frameid):
+        raise NotImplementedError()
+
         measBuf = io.StringIO()
         new = matchCatalog.dropna(thresh=6)
         new.to_csv(measBuf, sep=',', index=False, header=False)
@@ -150,7 +116,7 @@ class NajaVenator(object):
 
 class CobraTargetTable(object):
     def __init__(self, visitid, tries, calibModel, designID, goHome = False):
-        self._dbConn = opdb.OpDB(hostname='db-ics', dbname='opdb', username='pfs')
+        self._dbConn = opdb.OpDB()
         self.visitid = visitid
         self.tries = tries
         self.designID = designID
@@ -186,8 +152,6 @@ class CobraTargetTable(object):
                        'pfi_target_x_mm': [],
                        'pfi_target_y_mm': [],
                        'flags':[]
-                       #'motor_target_theta': [],
-                       #'motor_target_phi': [],
                        }
 
         for iteration in range(self.iteration):
@@ -206,10 +170,6 @@ class CobraTargetTable(object):
                     # Using cobra center for bad cobra targets
                     targetTable['pfi_target_x_mm'].append(self.calibModel.centers[idx].real)
                     targetTable['pfi_target_y_mm'].append(self.calibModel.centers[idx].imag)
-
-                    #targetTable['motor_target_theta'].append(0)
-                    #targetTable['motor_target_phi'].append(0)
-
                 else:
                     if self.goHome is True:
                         if iteration == 0:
@@ -226,13 +186,9 @@ class CobraTargetTable(object):
                         if iteration < 2:
                             targetTable['pfi_target_x_mm'].append(firstStepMove[goodIdx == idx].real[0])
                             targetTable['pfi_target_y_mm'].append(firstStepMove[goodIdx == idx].imag[0])
-                            #targetTable['motor_target_theta'].append(firstThetaAngle[goodIdx == idx][0])
-                            #targetTable['motor_target_phi'].append(firstPhiAngle[goodIdx == idx][0])
                         else:
                             targetTable['pfi_target_x_mm'].append(targetStepMove[goodIdx == idx].real[0])
                             targetTable['pfi_target_y_mm'].append(targetStepMove[goodIdx == idx].imag[0])
-                            #targetTable['motor_target_theta'].append(targetThetaAngle[goodIdx == idx][0])
-                            #targetTable['motor_target_phi'].append(targetPhiAngle[goodIdx == idx][0])
 
         self.dataTable = pd.DataFrame(targetTable)
 
@@ -240,4 +196,5 @@ class CobraTargetTable(object):
 
     def writeTargetTable(self):
         """Write self.dataTable to cobra_target table."""
-        self._dbConn.insert("cobra_target", self.dataTable)
+
+        self._dbConn.insert_dataframe("cobra_target", self.dataTable)
